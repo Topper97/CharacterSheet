2a77dbc2b4347826ab3046464c843a4cb7e6b34b
Chris <cbeau738@live.kutztown.edu>
2020-12-03 11:49:21 -0500

    Initial Commit

---
+++ CharacterSheet/CharacterSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterSheetApp.swift
+//  CharacterSheet
+//
+//  Created by Christopher Beaudoin on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct CharacterSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

eaa2896ea2377829885719b9cd5a3baeb276ca1e
Chris <cbeau738@live.kutztown.edu>
2020-12-03 13:23:05 -0500

    first real stuff

--- CharacterSheet/CharacterSheetApp.swift
+++ CharacterSheet/CharacterSheetApp.swift
@@ -1,7 +1,7 @@
 //
 //  CharacterSheetApp.swift
 //  CharacterSheet
-//
+//a9e32e33891f1200210074cc1ab3103f0debbf4c
 //  Created by Christopher Beaudoin on 12/3/20.
 //
 
--- CharacterSheet/ContentView.swift
+++ CharacterSheet/ContentView.swift
@@ -7,15 +7,142 @@
 
 import SwiftUI
 
+struct StrengthView: View {
+    var body: some View {
+        VStack {
+            Text("2")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("14")
+                .font(.title)
+            Text("Strength")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+                
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+struct DexterityView: View {
+    var body: some View {
+        VStack {
+            Text("5")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            
+            Text("20")
+                .font(.title)
+            Text("Dexterity")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+struct ConstitutionView: View {
+    var body: some View {
+        VStack {
+            Text("2")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("14")
+                .font(.title)
+            Text("Constitution")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+struct IntelligenceView: View {
+    var body: some View {
+        VStack {
+            Text("3")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("16")
+                .font(.title)
+            Text("Intelligence")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+struct WisdomView: View {
+    var body: some View {
+        VStack {
+            Text("1")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("13")
+                .font(.title)
+            Text("Wisdom")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+struct CharismaView: View {
+    var body: some View {
+        VStack {
+            Text("0")
+                //.padding()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("10")
+                .font(.title)
+            Text("Charisma")
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+                
+        }
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack () {
+            HStack(alignment: .firstTextBaseline) {
+                StrengthView()
+                DexterityView()
+                ConstitutionView()
+                IntelligenceView()
+                WisdomView()
+                CharismaView()
+            }
+            
+        
+            Text("Character sheet")
             .padding()
+            .foregroundColor(.red)
+            .font(.largeTitle)
+            
+            Spacer()
+        }
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        VStack () {
+            ContentView()
+        }
     }
 }
